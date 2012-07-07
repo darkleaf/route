ef2e0fae0c9a96482c0f7750fca7b7625dfe5d68
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2012-07-07 12:20:12 +0400

    initialize builder

--- lib/route/builder.rb
+++ lib/route/builder.rb
@@ -1,7 +1,15 @@
 class Route::Builder
+  attr_reader :routes
+
+  def initialize(&block)
+    @routes = Route::Routes.new
+    instance_eval &block
+  end
+
   class << self
-    def build
-      Route::Routes.new
+    def build(&block)
+      builder = new &block
+      builder.routes
     end
   end
 end
--- test/route/builder.rb
+++ test/route/builder.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class Route::BuilderTest < MiniTest::Unit::TestCase
   def test_builder_return_routes_instance
-    rs = Route::Builder.build
+    rs = Route::Builder.build do
+
+    end
 
     assert_kind_of Route::Routes, rs
   end
+
 end
